44af7e287efeac8621dc67f7a68035c18bb838eb
Tomasini Martin <martin.tomasini2@tridonic.com>
2016-07-07 14:10:38 +0200

    - Fixed missing and wrong header files

--- mbed-hal/internal_flash_api.h
+++ mbed-hal/internal_flash_api.h
@@ -25,9 +25,9 @@ int WriteFlash(uint32_t pui32Address, const uint8_t* pData, uint32_t ui32DataSiz
 int ReadFlash(uint32_t pui32Address, uint8_t* pData, uint32_t ui32DataSize);
 int EraseSectorByNumber(uint16_t ui16Sector);
 int EraseSectorByAddress(uint32_t ui32Address);
-int GetNumOfSectors(void);
-void GetSectorStartAdd(uint32_t ui32Sector, uint32_t* pui32Address);
-void GetSectorEndAdd(uint32_t ui32Sector, uint32_t* pui32Address);
+int GetNumOfSectors(uint32_t* pui32NumOfVectors);
+int GetSectorStartAdd(uint32_t ui32Sector, uint32_t* pui32Address);
+int GetSectorEndAdd(uint32_t ui32Sector, uint32_t* pui32Address);
 
 
 #endif /* YOTTA_MODULES_MBED_HAL_MBED_HAL_INTERNAL_FLASH_API_H_ */
